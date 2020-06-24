6c8e4ecdade05f3ddce307c14aeba70339458b7f
nbrette <62093583+nbrette@users.noreply.github.com>
2020-06-24 20:37:42 +0200

    Update gsb.sql

--- gsb.sql
+++ gsb.sql
@@ -1,11 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.9.2
--- https://www.phpmyadmin.net/
---
--- Hôte : 127.0.0.1:3306
--- Généré le :  mer. 24 juin 2020 à 18:32
--- Version du serveur :  8.0.18
--- Version de PHP :  7.3.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
